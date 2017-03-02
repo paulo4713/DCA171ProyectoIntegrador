16e7c04d55fc9127be9df1c555b240c5b846db04
Paulo Rosas <paulo4713@hotmail.com>
2017-03-02 08:27:13 -0500

    agregar txt´s y crea clase logica

---
+++ src/Logica.java
@@ -0,0 +1,14 @@
+import processing.core.PApplet;
+
+public class Logica {
+	PApplet app;
+	
+	public Logica(PApplet app){
+		
+		
+	}
+	
+	public void ejecutar(){
+		
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
-public class Main extends PApplet {
+import processing.core.PApplet;
 
+public class Main extends PApplet {
+	private Logica log;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		PApplet.main("Main");
 		System.out.println("holi");
 	}
+	
+	public void setup() {
+		log = new Logica(this);
+	}
 
+	public void draw() {
+
+		log.ejecutar();
+
+	}
+	public void keyPressed(){
+		System.out.println("hola");
+	}
+	
+	public void mousePressed(){
+		System.out.println("hola");
+	}
 }
