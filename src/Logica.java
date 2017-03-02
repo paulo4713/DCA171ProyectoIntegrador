26806760865d8757f766057f90047e3e9ccbd25e
Paulo Rosas <paulo4713@hotmail.com>
2017-03-02 08:50:37 -0500

    cargar txt y crea clase persona vacia

--- src/Logica.java
+++ src/Logica.java
@@ -1,14 +1,31 @@
+import java.util.TreeSet;
+
 import processing.core.PApplet;
 
 public class Logica {
 	PApplet app;
+	private String txtNom[];
+	private String txtDat[];
+	private String txtCol[];
+	private TreeSet<Persona> personas;
+	
 	
 	public Logica(PApplet app){
-		
-		
+		this.app=app;
+		cargarTxts();
 	}
 	
 	public void ejecutar(){
 		
+	}
+	
+	public void pintarPersonas(){
+		
+	}
+	
+	public void cargarTxts() {
+		txtNom = app.loadStrings("../data/nombres.txt");
+		txtDat = app.loadStrings("../data/datos.txt");
+		txtCol = app.loadStrings("../data/colores.txt");
 	}
 }
